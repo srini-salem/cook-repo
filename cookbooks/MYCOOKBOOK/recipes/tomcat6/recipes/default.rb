825ab2022cc794edb3156cc756f146fc5f0dc86e
srini-satyam <v_srinivasan@mahindrasatyam.com>
2013-03-11 18:14:41 +0530

    Update default.rb

--- cookbooks/MYCOOKBOOK/recipes/tomcat6/recipes/default.rb
+++ cookbooks/MYCOOKBOOK/recipes/tomcat6/recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: tomcat6
 # Recipe:: default
 #
-# Copyright 2012, FOX Sports 2Go - Operations
+# 
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
